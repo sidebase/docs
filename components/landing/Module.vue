621c2a2fdbf441dd61308e31248e88aa645167ad
Zoey <zoeykaiser8@gmail.com>
2022-12-08 17:46:47 +0100

    fix: modules section in light mode (#33)

--- components/landing/Module.vue
+++ components/landing/Module.vue
@@ -12,12 +12,12 @@ defineProps({
     <div class="card flow-root rounded-lg px-8 pb-8 xl:h-[250px]">
       <div class="-mt-6">
         <div>
-          <span class="inline-flex items-center justify-center rounded-xl bg-purple-900 p-4 shadow-lg">
-            <i class="fa text-2xl dark:text-white" :class="icon" />
+          <span class="inline-flex items-center justify-center rounded-xl bg-purple-400 dark:bg-purple-900 p-4 shadow-lg">
+            <i class="fa text-2xl text-white" :class="icon" />
           </span>
         </div>
-        <h3 class="mt-8 text-xl font-semibold leading-8 tracking-tight text-gray-100">{{ name }}</h3>
-        <p class="mt-2 text-base leading-7 text-gray-400">
+        <h3 class="mt-8 text-xl font-semibold leading-8 tracking-tight dark:text-gray-100">{{ name }}</h3>
+        <p class="mt-2 text-base leading-7 dark:text-gray-400">
           {{ description }}
         </p>
       </div>
@@ -36,8 +36,8 @@ defineProps({
 .card:hover {
   box-shadow: inset 5px 5px 5px rgba(0, 0, 0, 0.2),
   inset -5px -5px 15px rgba(255, 255, 255, 0.3),
-  5px 5px 15px rgba(0, 0, 0, 0.8), -5px -5px 15px rgba(255, 255, 255, 0.4);
+  5px 5px 15px rgba(0, 0, 0, 0.5), -5px -5px 15px rgba(255, 255, 255, 0.4);
 
-  @apply bg-purple-700/10;
+  @apply dark:bg-purple-700/10;
 }
 </style>
