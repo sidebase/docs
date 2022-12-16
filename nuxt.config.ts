73860ed54b51737880060e31091c0205138ca9ae
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-12-16 15:22:00 +0100

    Added Algolia Secrets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,14 @@ export default defineNuxtConfig({
       plausible: {
         domain: 'sidebase.io',
       },
+      algolia: {
+        applicationId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_API_KEY,
+        langAttribute: 'lang',
+        docSearch: {
+          indexName: 'sidebase'
+        }
+      }
     },
   },
 })
