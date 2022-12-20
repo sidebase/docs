a7ee863cae54e303ee766cbb5faf892a2dc913c0
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-12-20 12:31:00 +0100

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,13 @@ export default defineNuxtConfig({
         }
       }
     },
+    algolia: {
+      applicationId: process.env.ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_API_KEY,
+      langAttribute: 'lang',
+      docSearch: {
+        indexName: 'sidebase'
+      }
+    }
   },
 })
