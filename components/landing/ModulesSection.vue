270d5c103192721dbc153d3e8292f3fd2d2b86b6
Zoey Kaiser <zoeykaiser8@gmail.com>
2023-01-31 20:00:10 +0100

    Fixed modules navbar in light mode

--- components/landing/ModulesSection.vue
+++ components/landing/ModulesSection.vue
@@ -3,7 +3,7 @@ import Module from "./Module.vue";
 </script>
 
 <template>
-  <div class="grid xl:grid-cols-3 gap-10">
+  <div class="grid xl:grid-cols-4 gap-10">
     <Module
       name="nuxt-auth"
       icon="heroicons-solid:lock-closed"
@@ -21,6 +21,12 @@ import Module from "./Module.vue";
         icon="heroicons-solid:document-check"
         link="/nuxt-parse"
         description="A nuxt focused package to make data validation and parsing easy."
+    />
+    <Module
+        name="nuxt-pdf"
+        icon="heroicons-solid:document"
+        link="/nuxt-pdf"
+        description="A nuxt focused package to quickly export html pages as a pdf."
     />
   </div>
 </template>
