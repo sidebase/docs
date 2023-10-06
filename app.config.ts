d1e4cbdca5eec7514058472c3acefe999c9bacfc
Zoey <zoeykaiser8@gmail.com>
2023-10-06 10:47:23 +0200

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -37,7 +37,7 @@ export default defineAppConfig({
         },
         {
           text: 'Jobs',
-          href: 'https://sidestream.jobs.personio.de/',
+          href: 'https://www.sidestream.tech/team#jobs',
         }
       ]
     }
