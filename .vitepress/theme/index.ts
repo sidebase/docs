55ae80d04ae55701992fd5f7b700f82af295e775
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-05-10 15:36:35 +0200

    reverted changes to home

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,12 +2,15 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import Layout from './Layout.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => h(Layout, null, {}),
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
   enhanceApp({ app, router, siteData }) {
     // ...
   }
--- index.md
+++ index.md
@@ -1,6 +1,6 @@
 ---
 # https://vitepress.dev/reference/default-theme-home-page
-layout: landing
+layout: home
 
 hero:
   name: "sidebase"
