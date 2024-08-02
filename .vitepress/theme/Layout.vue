2953378958aae585536b90302ec08fac5059c9ce
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-08-02 09:38:03 +0200

    Improved Layout to not remove components on page switch

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,38 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+import GithubStarsButton from './components/GithubStarsButton.vue'
+import Banner from './components/Banner.vue'
+import Terminal from './components/Terminal/Terminal.vue'
+import SpinningGradient from './components/SpinningGradient.vue'
+
+const { Layout } = DefaultTheme
+
+// Banner Configuration
+const isBannerEnabled = true
+const bannerConfig = {
+  // Leave text empty to disable the banner
+  text: '✨ NuxtAuth v0.8.0 has been released! ✨',
+  button: {
+    href: 'https://github.com/sidebase/nuxt-auth/releases/tag/0.8.0',
+    text: 'View release notes'
+  }
+}
+</script>
+
+<template>
+  <Layout>
+    <template #nav-bar-content-after>
+      <GithubStarsButton owner="sidebase" repo="sidebase" />
+    </template>
+
+    <template #home-hero-image>
+      <Terminal />
+    </template>
+
+    <template #home-hero-before>
+      <Banner v-if="isBannerEnabled" v-bind="bannerConfig" />
+      <SpinningGradient />
+    </template>
+  </Layout>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,20 +1,9 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
 
+// Styles
 import './style.css'
 
-import Terminal from './components/Terminal/Terminal.vue'
-import SpinningGradient from './components/SpinningGradient.vue'
-
-export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // See all available slots: https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'home-hero-image': h(Terminal),
-      'home-hero-before': h(SpinningGradient)
-    })
-  },
-} satisfies Theme
+export default { extends: DefaultTheme, Layout } satisfies Theme
