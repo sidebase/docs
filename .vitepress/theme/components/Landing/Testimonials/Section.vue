e0fa954bb69e8e4f16d6157feea9d5d4fe7b53d7
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-08-19 13:14:14 +0200

    Added Twitter + Github Links

--- .vitepress/theme/components/Landing/Testimonials/Section.vue
+++ .vitepress/theme/components/Landing/Testimonials/Section.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+import XIcon from '../Icons/X.vue'
+import GithubIcon from '../Icons/Github.vue'
+
 import { testimonials } from './data'
 </script>
 
@@ -23,13 +26,33 @@ import { testimonials } from './data'
         <hr class="my-3 border-zinc-200 dark:border-zinc-700">
         <div class="flex items-center gap-2">
           <img :src="testimonial.avatar" class="ring-green-500 h-12 w-12 rounded-full p-1 ring-2 dark:ring-green-700" alt="Avatar">
-          <div class="leading-3">
+          <div class="grow leading-3">
             <h1 class="text-lg text-gray-700 dark:text-gray-100">
               {{ testimonial.name }}
             </h1>
             <p v-if="testimonial.position" class="text-xs font-semibold text-gray-400">
               {{ testimonial.position }}
             </p>
+          </div>
+          <div class="flex items-center gap-1 self-end">
+            <a
+              v-if="testimonial.twitterLink"
+              :href="testimonial.twitterLink"
+              target="_blank"
+              rel="noreferrer noopener"
+              class="p-1 bg-green-300 rounded cursor-pointer hover:bg-green-400 dark:bg-green-800 dark:hover:bg-green-700"
+            >
+              <XIcon />
+            </a>
+            <a
+              v-if="testimonial.githubLink"
+              :href="testimonial.githubLink"
+              target="_blank"
+              rel="noreferrer noopener"
+              class="p-1 bg-green-300 rounded cursor-pointer hover:bg-green-400 dark:bg-green-800 dark:hover:bg-green-700"
+            >
+              <GithubIcon />
+            </a>
           </div>
         </div>
       </div>
